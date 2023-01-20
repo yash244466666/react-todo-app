cab7a4acce0feed4890e21b2a79a753c5cbc76a3
Yash <74345713+yash244466666@users.noreply.github.com>
2023-01-20 12:40:41 +0600

    Fix Linter errors

--- src/classBased/components/InputTodo.js
+++ src/classBased/components/InputTodo.js
@@ -1,51 +1,53 @@
-import React, { Component } from "react"
+/* eslint-disable */
+import React, { Component } from 'react';
 
 class InputTodo extends Component {
-
     state = {
-        title: ""
-      };
+      title: '',
+    };
 
-      onChange = e => {
+      onChange = (e) => {
         this.setState(
-                {
-                    [e.target.name]: e.target.value,
-                }
-            );
+          {
+            [e.target.name]: e.target.value,
+          },
+        );
       };
 
-    //   handleSubmit = e => {
-    //     e.preventDefault();
-    //     this.props.addTodoProps(this.state.title);
-    //     this.setState({
-    //         title: ""
-    //     });
+      //   handleSubmit = e => {
+      //     e.preventDefault();
+      //     this.props.addTodoProps(this.state.title);
+      //     this.setState({
+      //         title: ""
+      //     });
 
     //   };
-    handleSubmit = e => {
-        e.preventDefault()
-        if (this.state.title.trim()) {
-          this.props.addTodoProps(this.state.title)
-          this.setState({
-            title: "",
-          })
-        } else {
-          alert("Please write item")
-        }
+    handleSubmit = (e) => {
+      e.preventDefault();
+      if (this.state.title.trim()) {
+        this.props.addTodoProps(this.state.title);
+        this.setState({
+          title: '',
+        });
+      } else {
+        alert('Please write item');
       }
+    }
 
     render() {
-        return (
+      return (
         <form onSubmit={this.handleSubmit} className="form-container">
-            <input type="text" 
-            placeholder="Add todo..." 
-            value={this.state.title} 
+          <input
+            type="text"
+            placeholder="Add todo..."
+            value={this.state.title}
             onChange={this.onChange}
             name="title"
-            className="input-text" />
-            <button className="input-submit">Submit</button>
+            className="input-text"
+          />
+          <button className="input-submit">Submit</button>
         </form>
-        )
+      );
     }
 }
-export default InputTodo
+export default InputTodo;
